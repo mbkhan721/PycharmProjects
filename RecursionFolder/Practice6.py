eac80871977bbde8bf21d47b44c5246c8519de15
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-11-27 16:50:10 -0500

    Recursion folder added

---
+++ RecursionFolder/Practice6.py
@@ -0,0 +1,7 @@
+""" Muhammad Khan
+
+1. Write a program that recursively counts down from n.
+a) Create a recursive function named countdown that accepts an
+integer n, and progressively decrements and outputs the value of n.
+b) Test your function with a few values for n."""
+
