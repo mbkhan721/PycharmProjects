dc8ebf21d8f4e4d09880c25c1f64029add8d6020
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-11-28 05:26:44 -0500

    Code added

--- RecursionFolder/Practice6.py
+++ RecursionFolder/Practice6.py
@@ -21,12 +21,31 @@ digits in a positive integer parameter.
 For example, a program that uses the function numEven follows.
 print(numEven(23)) # prints 1
 print(numEven(1212)) # prints 2
-print(numEven(777)) # prints 0 
+print(numEven(777)) # prints 0 """
 
-def numEven():
+def numEven(n): #defining numEven Function
+
+    even_count = 0 #making initial count=0
+
+    while (n > 0): #checking input number greater than 0 or not
+        rem = n % 10 #slashing up inputted number into digits
+        if (rem % 2 == 0): #verifing digit is even or odd by dividing with 2.if remainder=0 then digit is even
+            even_count += 1 #counting the even digits
+
+        n = int(n / 10)
+
+    print(even_count) #printing the even number count
+
+    if (even_count % 2 == 0): #exits the function
+        return 1
+    else:
+        return 0
 
 
+numEven(int(input("Enter to find out even numbers: "))) #inputing number
+print()
 
+"""
 print("Number 2:", numEven(23))
 print("Number 2:", numEven(1212))
 print("Number 2:", numEven(777))
