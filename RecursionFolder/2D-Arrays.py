f672d85146000c1abac9ac49069c5d0801c8014b
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-11-29 06:44:42 -0500

    Code added

--- RecursionFolder/2D-Arrays.py
+++ RecursionFolder/2D-Arrays.py
@@ -12,3 +12,23 @@ for row in a:
     print('= '.join([str(elem) for elem in row]))
 print()
 
+"""3. Display an 8x8 checkerboard.
+
+X 0 X 0 X 0 X 0
+0 X 0 X 0 X 0 X
+X 0 X 0 X 0 X 0
+0 X 0 X 0 X 0 X
+X 0 X 0 X 0 X 0
+0 X 0 X 0 X 0 X
+X 0 X 0 X 0 X 0
+0 X 0 X 0 X 0 X
+"""
+y = 8
+z = [["X"] * y for k in range(y)]
+for k in range(y):
+    for b in range(y):
+        if k < b:
+            z[k][b] = 0
+        else:
+            z[k][j] = "X"
+    print(([str(elem) for elem in row]))
