ec17faa91238c477d835d517fbd659626ce4106e
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-12-09 02:42:30 -0500

    Code added

--- pythonProject/Graphics-2.py
+++ pythonProject/Graphics-2.py
@@ -1,9 +1,19 @@
 import turtle
 t = turtle.Turtle()
-style = ("Calibri")
+# t.style = (Corbel)
+t.pensize(4)
 
 HEIGHT = 100
-WIDTH = 40
+t.width = 40
+
+def mov(x, y):
+    t.up()
+    t.setposition(0, 0)
+    t.setheading(90)
+    t.left(90)
+    t.forward(x)
+    t.right(90)
+
 
 def letterM():
     t.goto(0, HEIGHT)
@@ -11,11 +21,67 @@ def letterM():
     t.goto(40, HEIGHT)
     t.goto(40, 0)
 
+
+def A(size):
+    t.right(16)
+    t.forward(size)
+    t.right(150)
+    t.forward(size)
+    t.backward(size / 2)
+    t.right(105)
+    t.forward(size / 3)
+
+
+def B():
+    t.forward(60)
+    t.right(90)
+    for i in range(18):
+        t.right(9)
+        t.forward(3)
+        for i in range(18):
+            t.right(13)
+            t.backward(3)
+
+
+def D():
+    t.forward(60)
+    t.right(90)
+    t.forward(9)
+    for i in range(13):
+        t.right(13)
+        t.forward(7)
+
+
+def H():
+    t.forward(60)
+    t.backward(30)
+    t.right(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(30)
+    t.backward(60)
+
+
 letterM()
 t.setheading(10)
 t.penup()
 t.forward(20)
 t.stamp()
+
+mov(325, 400)
+H()
+mov(220, 400)
+A()
+mov(200, 400)
+letterM()
+mov(180, 400)
+letterM()
+mov(160, 400)
+letterM()
+mov(140, 400)
+A()
+mov(120, 400)
+# 6;34
 
 def letterD():
     t.goto(100, HEIGHT)
@@ -30,7 +96,7 @@ turtle.done()
 
 
 
-"""
+
 # This is D
 turtle.circle(100, 180) #180 degrees of a circle radius 100
 turtle.left(90)
@@ -54,4 +120,4 @@ turtle.setheading(32)
 turtle.penup()
 turtle.forward(160)
 turtle.mainloop()
-"""
+
--- pythonProject/GraphicsHW.py
+++ pythonProject/GraphicsHW.py
@@ -1,8 +1,10 @@
 import turtle
 t = turtle.Turtle()
+t.width(2)  # 1:12
 
-HEIGHT = 100
+HEIGHT = 50 # Letter D wont adjust to the height as i commented out the first one
 WIDTH = 40
+
 
 def letterM():
     t.goto(0, HEIGHT)
@@ -11,19 +13,144 @@ def letterM():
     t.goto(40, 0)
 
 
-def letteri():
-    t.setheading(90)
-    t.forward(50)
-    t.penup()
-    t.forward(20)
-    t.setheading(0)
-    t.pendown()
-    t.circle(5)
-# to add space, pick up turtle, move
+def letterU():
+    t.goto(50, HEIGHT)
+    t.goto(50, 0)
+    t.goto(90, 0)
+    t.goto(90, HEIGHT)
+
+
+def letterH():
+    t.goto(100, 0)
+    t.goto(100, HEIGHT / 2)
+    t.goto(140, HEIGHT / 2)
+    t.goto(140, HEIGHT)
+    t.goto(140, 0)
+
+
+def letterA():
+    t.goto(150, HEIGHT)
+    t.goto(190, HEIGHT)
+    t.goto(190, HEIGHT / 2)
+    t.goto(150, HEIGHT / 2)
+    t.goto(190, HEIGHT / 2)
+    t.goto(190, 0)
+
+
+def letterM2():
+    t.goto(200, HEIGHT)
+    t.goto(220, 0)
+    t.goto(240, HEIGHT)
+    t.goto(240, 0)
+
+
+def letterM3():
+    t.goto(250, HEIGHT)
+    t.goto(270, 0)
+    t.goto(290, HEIGHT)
+    t.goto(290, 0)
+
+
+def letterA2():
+    t.goto(300, HEIGHT)
+    t.goto(340, HEIGHT)
+    t.goto(340, HEIGHT / 2)
+    t.goto(300, HEIGHT / 2)
+    t.goto(340, HEIGHT / 2)
+    t.goto(340, 0)
+
+"""
+def letterD():
+    t.goto(350, HEIGHT)
+    t.goto(390, HEIGHT)
+    t.goto(390, 0)
+    t.goto(350, 0)
+    t.goto(390, 0)
+"""
+
 letterM()
-t.setheading(10)
+t.setheading(0)
 t.penup()
-t.forward(20)
+t.forward(10)
 t.pendown()
-letterM()
-turtle.mainloop()
+
+
+letterU()
+t.setheading(0)
+t.penup()
+t.forward(10)
+t.pendown()
+
+
+letterH()
+t.setheading(0)
+t.pendown()
+t.penup()
+t.forward(10)
+t.pendown()
+
+
+letterA()
+t.setheading(0)
+t.penup()
+t.forward(10)
+t.pendown()
+
+
+letterM2()
+t.setheading(0)
+t.penup()
+t.forward(10)
+t.pendown()
+
+
+letterM3()
+t.setheading(0)
+t.penup()
+t.forward(10)
+t.pendown()
+
+
+letterA2()
+t.setheading(0)
+t.penup()
+t.forward(10)
+t.pendown()
+
+# First method of letter D
+"""
+letterD()
+t.circle(WIDTH, HEIGHT)
+t.left(90)
+t.penup()
+t.forward(30)
+t.pendown()
+"""
+# I used two methods for letter D
+
+# Genuine looking letter D
+t.circle(26, 180)
+t.left(90)
+t.forward(52)
+t.penup()
+t.left(90)
+t.forward(60)
+
+# Letter B
+t.pendown()
+t.forward(25)
+t.circle(13,180)
+t.forward(25)
+t.setheading(0)
+t.forward(26)
+t.circle(13,180)
+t.forward(26)
+t.setheading(270)
+t.forward(52)
+t.setheading(32)
+t.penup()
+t.forward(60)
+
+
+turtle.done()
+# turtle.mainloop()
