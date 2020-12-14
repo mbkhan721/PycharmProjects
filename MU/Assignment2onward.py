ccda5612657fb3aad494901c11a7c47f3b933660
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-12-14 03:24:24 -0500

    Code added

--- MU/Assignment2onward.py
+++ MU/Assignment2onward.py
@@ -15,3 +15,10 @@ x = float(hrs) * float(rate) # method 2
 
 #print("Pay:", pay * float(rate)) # method 1
 print("Pay:",x) # method 2
+
+
+for i in range(5):
+    print(i)
+    if i > 2:
+        print("Bigger than 2")
+    print("Done with i", i)
